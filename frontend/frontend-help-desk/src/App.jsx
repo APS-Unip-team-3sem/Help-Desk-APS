f5ce34014dde49448bf596b2ed3f1bc0a10e23ad
Letícia Milan <leticiamilan.dev@gmail.com>
2024-04-25 22:42:36 -0300

    🎨 feat: add configs tailwind e página de login

--- frontend/frontend-help-desk/src/App.jsx
+++ frontend/frontend-help-desk/src/App.jsx
@@ -2,13 +2,13 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Login from '../src/layouts/Auth';
+import SignIn from '../src/views/auth/SignIn';
 
 const App = () => {
   return (
     <div>
-      <h1>Teste</h1>
-      <Login />
+      {/* <h1>Teste</h1> */}
+      <SignIn />
     </div>
   );
 };
--- frontend/frontend-help-desk/src/main.jsx
+++ frontend/frontend-help-desk/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
