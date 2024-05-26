6e30186b5ce1e217501f73cf3419c98124d6462d
Letícia Milan <leticiamilan.dev@gmail.com>
2024-05-25 23:20:13 -0300

    ✨ feat: add fotinha no comentario do usuario

--- frontend/frontend-help-desk/src/components/Ticket/TicketDetails.tsx
+++ frontend/frontend-help-desk/src/components/Ticket/TicketDetails.tsx
@@ -225,8 +225,9 @@ const TicketDetails: React.FC = () => {
                     <div className="p-8">
                         <h2 className="text-xl font-semibold mb-4"></h2>
                         {comments.map((comment, index) => (
-                            <div key={index} className="flex items-start gap-2.5 mb-4 bg-slate-100 rounded-md">
-                                <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                            <div key={index} className="flex items-start py-2 px-2 gap-2.5 mb-4 bg-slate-100 rounded-md">
+                                <span className="flex items-center justify-center w-8 h-8 rounded-full bg-white text-center">{comment.usuarioModel.nome.charAt(0).toUpperCase()}</span>
+                                <div className="flex flex-col w-full max-w-[320px] leading-1.5 px-2 py-1 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
                                     <div className="flex items-center space-x-2 rtl:space-x-reverse">
                                         <span className="text-sm font-semibold text-gray-900 dark:text-white">{comment.usuarioModel.nome}</span>
                                         <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
